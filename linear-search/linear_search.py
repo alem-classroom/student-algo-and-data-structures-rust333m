67edbcd941830eeab49b3792a89ac3216b4896c0
rust333m <47534213+rust333m@users.noreply.github.com>
2020-08-04 17:55:46 +0600

    Update linear_search.py

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -2,7 +2,6 @@ def linear_search(lst, to_find):
 	num = -1
 	for i in range(len(lst)):
 		if to_find == lst[i]:
-			num = lst[i]
+			num = i
 			return num
 	return num
-print(linear_search([1,2,3,4],5))
