0ed82766802b35c15657762b486abc5700155a88
rust333m <47534213+rust333m@users.noreply.github.com>
2020-08-04 17:50:46 +0600

    Update linear_search.py

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,4 +1,7 @@
 def linear_search(lst, to_find):
-  # search for the element to_find inside lst
-  # if found, return index of element
-  # else return -1
+	num = -1
+	for i in range(len(lst)):
+		if to_find == lst[i]:
+			num = lst[i]
+			return num
+	return num
